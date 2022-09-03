47d6da296023421cf76495c8f247c4bb634f919a
MaxPrehoda <maxprehoda@gmail.com>
2022-09-03 15:16:13 -0700

    Custom button and heart svg

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ function App() {
       <header className="App-header">
         <div className="absolute top-0">
           <h1 className="mt-4">Down the Rabbit Hole 🐇</h1>
-          <p className="relative text-base max-h-[12px] overflow-none mt-1 pb-1">
-            Made with{" "}
+          <p className="relative text-base max-h-[12px] overflow-none mt-1 pb-1 flex sm:pl-0 md:pl-8 lg:pl-16">
+            Made with&nbsp;
             <span role="img" aria-label="heart">
-              ❤️
-            </span>{" "}
-            by <a href="https://maxprehoda.info/" className="portfolio-link">Maxwell Prehoda</a>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#6366f1
+" viewBox="0 0 24 24" stroke-width="1.5" stroke="#6366f1
+" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+</svg>
+
+            </span>&nbsp;
+            by<a href="https://maxprehoda.info/" className="portfolio-link">&nbsp; Maxwell Prehoda</a>
           </p>
         </div>
         <div className="card bg-gray-800 rounded-md p-4 pt-6">
@@ -27,7 +32,7 @@ function App() {
         </p>
         </div>
         <div className="card bg-gray-800 rounded-md -mt-6 pl-6 pr-6 mb-12">
-        <p className="text-3xl mb-6 mt-6">How can we find the rabbit on the first pass every time?</p>
+        <p className="text-3xl mb-6 mt-6">How can we find the rabbit on the first pass through every time?</p>
         </div>
         <Game />
         <p className="text-xl max-w-[75ch] mt-8">
@@ -206,7 +211,10 @@ class StartStopButton extends React.Component {
   render() {
     return (
       <div className="mb-6 space-x-6" onClick={this.onTrigger}>
-        <button type="button">▶️</button>
+        <button type="button" className="bg-indigo-500 rounded-md p-2"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.75" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+</svg>
+</button>
       </div>
     );
   }
@@ -226,7 +234,7 @@ class Answer extends React.Component {
   render() {
     return (
       <div className="pt-8">
-      <button className="absolute bg-indigo-600 rounded-md pl-3 pr-3 text-md -ml-28 z-10">Reveal Answer</button>
+      <button className="absolute bg-indigo-500 rounded-md pl-3 pr-3 text-md -ml-28 z-10">Reveal Answer</button>
       <div className="card bg-gray-800 rounded-md p-4 blur-xl">
       <p className="text-xl max-w-full">
         There are 100 holes 🕳️ in a line, and there's a rabbit 🐰 hiding in
